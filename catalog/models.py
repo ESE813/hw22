fd14f6dc4493d925723909aade0febba7b9dc611
Elena <elenaolgaegorova1994@gmail.com>
2026-09-11 16:11:50 +0300

    migrate

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=150)
+    photo = models.ImageField(upload_to='photos/')
+    category = models.TextField()
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='products')
+
+
+
+
+    def __str__(self):
+        return f"{self.name} {self.description}"
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+
+
+
+
+class Category(models.Model):
+    name = models.CharField
+    description = models.TextField
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='categories')
+
+    def __str__(self):
+        return f"{self.name} {self.description}"
+
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+
+
+
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
 from catalog.views import home, contacts
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("home/", home, name="home"),
-    path("contacts/", contacts, name="contacts"),
+    path('home/', home, name='home'),
+    path('contacts/', contacts, name='contacts'),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
